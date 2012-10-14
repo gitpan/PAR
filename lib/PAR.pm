5601406aefc8aec7bab752b0b30842d6ac320a84
Roderich Schupp <rschupp@cpan.org>
2012-10-14 22:45:17 +0000

    Import of RSCHUPP/PAR-1.006 from CPAN.
    gitpan-cpan-distribution: PAR
    gitpan-cpan-version:      1.006
    gitpan-cpan-path:         RSCHUPP/PAR-1.006.tar.gz
    gitpan-cpan-author:       RSCHUPP
    gitpan-cpan-maturity:     released

--- lib/PAR.pm
+++ lib/PAR.pm
@@ -1,5 +1,5 @@
 package PAR;
-$PAR::VERSION = '1.005';
+$PAR::VERSION = '1.006';
 
 use 5.006;
 use strict;
@@ -537,7 +537,7 @@ sub _import_repository {
     my $obj;
 
     # Support existing clients passed in as objects.
-    if (ref($url) and UNIVERSAL::isa($obj, 'PAR::Repository::Client')) {
+    if (ref($url) and UNIVERSAL::isa($url, 'PAR::Repository::Client')) {
         $obj = $url;
     }
     else {
@@ -550,7 +550,7 @@ sub _import_repository {
     }
 
     if (exists($opt->{fallback}) and not $opt->{fallback}) {
-        push @PriorityRepositoryObjects, $obj; # repository beats local stuff
+        unshift @PriorityRepositoryObjects, $obj; # repository beats local stuff
     } else {
         push @RepositoryObjects, $obj; # local stuff beats repository
     }
